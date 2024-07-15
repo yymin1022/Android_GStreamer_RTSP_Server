fd273f0bd87db3f8dee1f9645fb3c7ecfd77dfdc
yymin1022 <yymin1022@gmail.com>
2024-07-15 09:49:24 +0900

    Update Project : Added NDK Basic Structure

---
+++ app/src/main/cpp/gstreamer-rtsp.cpp
@@ -0,0 +1,14 @@
+#include <jni.h>
+#include <string>
+
+#include <android/log.h>
+
+#define LOG_TAG "NDK LOG"
+#define LOG_I(...) __android_log_print(ANDROID_LOG_INFO, LOG_TAG, __VA_ARGS__)
+#define LOG_E(...) __android_log_print(ANDROID_LOG_ERROR, LOG_TAG, __VA_ARGS__)
+
+extern "C" JNIEXPORT void JNICALL
+Java_com_geeksloft_rtsp_MainActivity_startRtspServer(JNIEnv* env, jobject)
+{
+    LOG_I("NDK Function Loaded!!");
+}
