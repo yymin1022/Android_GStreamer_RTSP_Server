f1cbcff412c89f633fc090ad506da84d94bf4e6a
yymin1022 <yymin1022@gmail.com>
2024-07-15 13:36:09 +0900

    Update NDK : Added Basic GST RTSP Functions

--- app/src/main/cpp/gstreamer-rtsp.cpp
+++ app/src/main/cpp/gstreamer-rtsp.cpp
@@ -3,12 +3,24 @@
 
 #include <android/log.h>
 #include <gst/gst.h>
+#include <gst/rtsp-server/rtsp-server.h>
 
 #define LOG_TAG "NDK LOG"
 #define LOG_I(...) __android_log_print(ANDROID_LOG_INFO, LOG_TAG, __VA_ARGS__)
 #define LOG_E(...) __android_log_print(ANDROID_LOG_ERROR, LOG_TAG, __VA_ARGS__)
 
 static GMainLoop *loop;
+static gboolean
+timeout (GstRTSPServer * server)
+{
+    GstRTSPSessionPool *pool;
+
+    pool = gst_rtsp_server_get_session_pool (server);
+    gst_rtsp_session_pool_cleanup (pool);
+    g_object_unref (pool);
+
+    return TRUE;
+}
 
 extern "C" JNIEXPORT void JNICALL
 Java_com_geeksloft_rtsp_GstreamerUtil_startRtspServer(JNIEnv* env, jobject)
@@ -17,6 +29,14 @@ Java_com_geeksloft_rtsp_GstreamerUtil_startRtspServer(JNIEnv* env, jobject)
     gst_init(NULL, NULL);
 
     loop = g_main_loop_new(NULL, FALSE);
+
+    GstRTSPServer *server = gst_rtsp_server_new();
+    GstRTSPMediaFactory *factory = gst_rtsp_media_factory_new();
+
+    gst_rtsp_media_factory_set_launch(factory, "( videotestsrc ! x264enc ! rtph264pay name=pay0 pt=96 )");
+    gst_rtsp_server_attach(server, NULL);
+
+    g_timeout_add_seconds(2, (GSourceFunc) timeout, server);
 
     g_main_loop_run(loop);
     LOG_I("Started RTSP Server");
